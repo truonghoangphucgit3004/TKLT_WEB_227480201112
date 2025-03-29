065bab8ffe90d668e0e91d884f3a9acdd38cd948
Truong Hoang Phuc <phuctruong.tpbl@gmail.com>
2025-03-30 01:23:02 +0700

    Bai tap chuong 5

---
+++ Chuong_5/Bai-2/B2b.php
@@ -0,0 +1,10 @@
+<?php
+    $hoten = $_GET["txthoten"];
+    $ngaysinh = $_GET["txtngaysinh"];
+    $lop = $_GET["txtlop"];
+    $diem = $_GET["txtdiem"];
+    echo "Họ và tên: $hoten <br>";
+    echo "Ngày sinh: $ngaysinh <br>";
+    echo "Lớp: $lop <br>";
+    echo "Điểm: $diem <br>";
+?>
---
+++ Chuong_5/Bai-2/index.php
@@ -0,0 +1,7 @@
+<form action="B2b.php" method="get">
+      Nhập họ và tên: <input type="text" name="txthoten" /> <br />
+      Nhập ngày sinh: <input type="text" name="txtngaysinh" /> <br />
+      Nhập lớp: <input type="text" name="txtlop" /> <br />
+      Nhập điểm: <input type="text" name="txtdiem" /> <br />
+      <input type="submit" value="XONG" />
+</form>
